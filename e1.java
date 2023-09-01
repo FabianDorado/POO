a5209ff361588d66753140a4e81f7ec5088a29d1
FabianDorado <142599451+FabianDorado@users.noreply.github.com>
2023-09-01 12:15:47 -0600

    Add files via upload

---
+++ e1.java
@@ -0,0 +1,21 @@
+package poo.unicauca.edu.co;
+
+public class e1 {
+    
+   String firstName = "Fabian";
+   String lastName = "Dorado";
+    
+   public int sum(int n1, int n2) {
+       return n1 + n2; 
+    }
+    
+   public String getFirstName() {
+    return firstName;
+}
+   
+   public String getLastName() {
+      return lastName;  
+   }
+}
+ 
+       
